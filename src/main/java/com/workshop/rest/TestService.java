3e652a6dd92a70e9630234ac8b24012d3b84149a
Eric Chan <eric.chan@bchydro.com>
2016-07-20 14:27:15 -0700

    first commit

--- src/main/java/com/workshop/rest/TestService.java
+++ src/main/java/com/workshop/rest/TestService.java
@@ -3,14 +3,35 @@ package com.workshop.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-@Path("/testservice")
+import com.google.gson.JsonObject;
+import com.workshop.utility.Utility;
+
+@Path("/rest")
 public class TestService {
 
     @GET
     @Produces("text/plain")    
+	@Path("/testservice")
     public String getMessage() {
         return "Hello there";
     }
+	
+	@GET
+    @Produces("text/plain")    
+	@Path("/myservice/{name}")
+    public String getName(@PathParam("name") String name) {
+        return "Hello " + name;
+    }
+	
+	@GET
+    @Produces("text/plain")    
+	@Path("/myjson")
+    public String getJson() {
+		JsonObject json=Utility.convertFileToJSON("test.json");
+        return json.toString();
+    }
+	
 }
